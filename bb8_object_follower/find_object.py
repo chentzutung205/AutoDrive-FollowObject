08f17a94d82c98f7f471405612cbe1d191539a39
Tzu-Tung Chen <tchen604@gatech.edu>
2023-09-27 14:21:06 -0400

    TG find_object is working!

--- bb8_object_follower/find_object.py
+++ bb8_object_follower/find_object.py
@@ -1,9 +1,11 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import CompressedImage, Image
+from sensor_msgs.msg import CompressedImage
 from geometry_msgs.msg import Point
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy, QoSReliabilityPolicy, QoSHistoryPolicy
 from cv_bridge import CvBridge
+
+import sys
 
 import cv2
 import numpy as np
@@ -26,7 +28,7 @@ class ImageSubscriber(Node):
         self._titleOriginal = self.get_parameter('window_name').value  # Image Window Title
 
         # Only create image frames if we are not running headless (_display_image sets this)
-        if self._display_image:
+        if (self._display_image):
             # Set Up Image Viewing
             cv2.namedWindow(self._titleOriginal, cv2.WINDOW_AUTOSIZE)  # Viewing Window
             cv2.moveWindow(self._titleOriginal, 50, 50)  # Viewing Window Original Location
@@ -47,15 +49,22 @@ class ImageSubscriber(Node):
             image_qos_profile)
         self._image_subscriber  # Prevents unused variable warning.
 
-        self._coordinate_publisher = self.create_publisher(Point, 'pixel_coordinate', 10)
-        self._image_publisher = self.create_publisher(Image, 'processed_image', 10)
+        self._coordinate_publisher = self.create_publisher(Point, '/point_coordinate', 10)
 
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self._timer_callback)
+
+    def publish_point(self, circles):
+       if circles is not None:
+           for circle in circles[0, :]:
+               point = Point()
+               point.x = float(circle[0])
+               point.y = float(circle[1])
+               point.z = 0.0
+               self._coordinate_publisher.publish(point)
 
     def _image_callback(self, CompressedImage):
         # The "CompressedImage" is transformed to a color image in BGR space and is store in "_imgBGR"
         self._imgBGR = CvBridge().compressed_imgmsg_to_cv2(CompressedImage, "bgr8")
+        # Image Processing to find circle
         imgGRAY = cv2.cvtColor(self._imgBGR, cv2.COLOR_BGR2GRAY)
         imgBLUR = cv2.medianBlur(imgGRAY, 5)
         circles = cv2.HoughCircles(imgBLUR, cv2.HOUGH_GRADIENT, 1, 80, param1=110, param2=63, minRadius=0, maxRadius=0)
@@ -64,32 +73,24 @@ class ImageSubscriber(Node):
             circles = np.uint16(np.around(circles))
             for circle in circles[0, :]:
                 # Print the pixel location
-                self._xc, self._yc, self._radius = circle[0], circle[1], circle[2]
+                xc, yc, r = circle[0], circle[1], circle[2]
                 # draw the outer circle (bounding boxes)
-                cv2.circle(self._imgBGR, (self._xc, self._yc), self._radius, (255, 0, 0), 7)
+                cv2.circle(self._imgBGR, (xc, yc), r, (255, 0, 0), 7)
                 # draw the center of the circle
-                cv2.circle(self._imgBGR, (self._xc, self._yc), 2, (0, 0, 255), 3)
-                # record the center of circle to Point
-                point = Point()
-                point.x = self._xc
-                point.y = self._yc
-                point.z = 0.0
+                cv2.circle(self._imgBGR, (xc, yc), 2, (0, 0, 255), 3)
 
-        if self._display_image:
+        if (self._display_image):
             # Display the image in a window
             self.show_image(self._imgBGR)
+            cv2.waitKey(1)
 
-    def _timer_callback(self,Point): #NEED TO FIXXX
-        # point = Point()
-        # point.x = circle[0]
-        # point.y = circle[1]
-        # point.z = 0.0
-        self.publisher_.publish(Point)
+        self.publish_point(circles)
+
 
     def show_image(self, img):
         cv2.imshow(self._titleOriginal, img)
         # Cause a slight delay so image is displayed
-        self._user_input = cv2.waitKey(50)  # Use OpenCV keystroke grabber for delay.
+        self._user_input = cv2.waitKey(10)  # Use OpenCV keystroke grabber for delay.
 
     def get_user_input(self):
         return self._user_input
@@ -100,7 +101,7 @@ def main():
     image_subscriber = ImageSubscriber()  # Create class object to be used.
 
     while rclpy.ok():
-        rclpy.spin_once(image_subscriber)  # Trigger callback processing.
+        rclpy.spin(image_subscriber)  # Trigger callback processing.
         if image_subscriber._display_image:
             if image_subscriber.get_user_input() == ord('q'):
                 cv2.destroyAllWindows()
